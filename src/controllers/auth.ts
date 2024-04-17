82f214e0bc4d8873cb6d4839dba6c71b8bcda78a
surge100 <zoqol@daum.net>
2024-04-17 13:19:05 +0900

    [➕] bcrypt, jsonwebtoken 종속성설치

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,4 +1,19 @@
 import { RequestHandler } from "express";
+import { prisma } from '../index';
+
+export const signup:RequestHandler = async (req,res) =>{
+    const { email, passowrd, name } = req.body;
+
+    let user = await prisma.user.findFirst({
+        where:{
+            email
+        }
+    });
+
+    if(user){
+        throw Error('해당 email의 사용자가 이미 존재합니다.');
+    }
+}
 
 export const login:RequestHandler = (req,res) => {
     res.send('Login works');
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
-import express, {Express, Request, Response, Router} from 'express'
+import express, {ErrorRequestHandler, Express, Request, Response, Router} from 'express'
 import * as dotenv from 'dotenv'
 import rootRouter from './routes';
+import { PrismaClient } from '@prisma/client';
+import { RequestHandler } from 'express';
 process.env.NODE_ENV == 'prod'? dotenv.config({path:'./.env.prod'}) : dotenv.config({path:'./.env.local'});
 
 const app:Express = express();
 
 app.use('/',rootRouter);
 
-app.get('/',(req:Request,res:Response) =>{
-    res.send('Working');
+app.use((error:Error, req:Request, res:Response)  =>{
+
 })
+ export const prisma = new PrismaClient({
+    log:['query']
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`App이 ${process.env.PORT}에서 작동 중 입니다.`);
